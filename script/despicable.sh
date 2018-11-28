0639b1794c628af88545c6308f1b9690fdd1baf2
Tuomas <tuomas.olkinuora@hotmail.fi>
2018-11-28 07:09:49 +0000

    MODIFIED despicable.sh

--- script/despicable.sh
+++ script/despicable.sh
@@ -5,10 +5,23 @@ echo "Keyboard map Finnish and installing salt and git"
 
 setxkbmap fi
 sudo apt-get update
-sudo apt-get -y install git salt-minion 
+sudo apt-get -y install git salt-minion salt-master
 
 echo "Minionizing"
 
-echo "master: 192.168.10.50" | sudo tee /etc/salt/minion
+echo "master: localhost" | sudo tee /etc/salt/minion
+
+echo "Retrieving salt from GitHub"
+
+cd /srv/
+sudo git clone https://github.com/AlfieLinux/salt.git
 sudo systemctl restart salt-minion
+cd /salt/
+
+echo "git settings"
+
+git config --global credential.helper "cache --timeout=3600"
+git config --global push.default simple
+git config --global user.email "terosstudent@example.com"
+git config --global user.name "Tuomas Olkinuora"
 
